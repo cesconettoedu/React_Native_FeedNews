a6797cb0c82516657651a7e1c82007ededcc6a2e
Eduardo <cesconettoedu@gmail.com>
2023-04-05 23:32:54 -0400

    feed news

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider, Box } from 'native-base';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Home from './src/pages'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NativeBaseProvider>
+    <Home />
+   </NativeBaseProvider>
   );
 }
 
